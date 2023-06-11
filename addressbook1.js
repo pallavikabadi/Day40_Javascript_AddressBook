4ed7eff9898d0b4f17e2f188b2a827274536b7ad
pallavi kabadi <pallavi08jadhav@gmail.com>
2023-06-11 17:35:26 +0530

    Added:Address Book in Javascript

---
+++ addressbook1.js
@@ -0,0 +1,18 @@
+UC1::
+//To create Person class
+class Person{
+    constructor(firstName,lastName,address,city,state,zip,phoneNumber,email){
+        this.firstName  = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+    display(){
+        console.log("First Name "+this.firstName+" Last Name "+this.lastName+" Address "+
+        this.address+" City "+this.city+" State "+this.state+" Zip "+this.zip+" PhoneNumber "+
+        this.phoneNumber+" Email "+this.email);
+    }
+}
